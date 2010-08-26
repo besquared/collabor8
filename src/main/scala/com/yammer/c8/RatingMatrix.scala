d9096720bd9665d5dbe07abae5c2a94cc1cc861a
Josh Ferguson <jferguson@yammer-inc.com>
2010-08-25 23:25:05 -0700

    changing rating matrix to not use kamikaze for now

--- src/main/scala/com/yammer/c8/RatingMatrix.scala
+++ src/main/scala/com/yammer/c8/RatingMatrix.scala
@@ -1,126 +1,154 @@
 package com.yammer.c8
 
-import java.util._
-import com.kamikaze.docidset.api._
-import com.kamikaze.docidset.impl._
-import com.kamikaze.docidset.utils._
+import scala.collection.mutable.Set
+import scala.collection.mutable.Seq
 import scala.collection.mutable.HashMap
-import org.apache.lucene.search.DocIdSetIterator
 
 object RatingMatrix {
-  val data = new HashMap[String, Int]
-  val indices = new HashMap[String, DocSet]
+  // userId:items => 1,2,3,...
+  // userId:<rating> => 1,3,...
+  val userIndex = new HashMap[String, Set[Int]]
   
-  // We need to look for existing rating in the matrix 
-  //  and swap the indices to the new value if they exist
+  // itemId:users => 1,2,3,...
+  // itemId:<rating> => 1,3,...
+  val itemIndex = new HashMap[String, Set[Int]]
   
-  def insert(user_id:Int, item_id:Int, rating:Int) {
-    // Class Information
-    insert("classes", rating)
-    
-    // Data
-    data.put(item_id.toString() + user_id.toString(), rating)
-    
-    // Item Indices
-    insert(item_id.toString() + ":users", user_id)
-    insert(item_id.toString() + ":" + rating.toString(), user_id)
-    
-    // User Indices
-    insert(user_id.toString() + ":items", item_id)
-    insert(user_id.toString() + ":" + rating.toString(), item_id)
+  // userId => itemId,rating,itemId,rating
+  val userRatings = new HashMap[String, Seq[Int]]
+  
+  // itemId => userId,rating,userId,rating
+  val itemRatings = new HashMap[String, Seq[Int]]
+  
+  def insert(userId:Int, itemId:Int, rating:Int) {
+    insertUserIndex(userId, itemId, rating)
+    insertItemIndex(userId, itemId, rating)
+    insertUserRating(userId, itemId, rating)
+    insertItemRating(userId, itemId, rating)
   }
   
-  def findClasses():Option[DocSet] = {
-    indices.get("classes")
+  def insertUserIndex(userId:Int, itemId:Int, rating:Int) {
+    val itemKey = userId.toString() + ":items"
+    val ratingKey = userId.toString() + ":" + rating.toString()
+    
+    List(itemKey, ratingKey).foreach(
+      key =>
+        userIndex.get(key) match {
+          case Some(userIdx) =>
+            userIdx + itemId
+          case None => 
+            userIndex.put(key, Set(itemId))
+        }
+    )
   }
   
-  def findRating(user_id:Int, item_id:Int):Option[Int] = {
-    data.get(item_id.toString() + user_id.toString())
+  def insertItemIndex(userId:Int, itemId:Int, rating:Int) {
+    val userKey = itemId.toString() + ":users"
+    val ratingKey = itemId.toString() + ":" + rating.toString()
+    
+    List(userKey, ratingKey).foreach(
+      key =>
+        itemIndex.get(key) match {
+          case Some(itemIdx) =>
+            itemIdx + userId
+          case None => 
+            itemIndex.put(key, Set(userId))
+        }
+    )
   }
+  
+  def insertUserRating(userId:Int, itemId:Int, rating:Int) {
+    val itemKey = itemId.toString()
+    
+    userRatings.get(itemKey) match {
+      case Some(userRatings) =>
+        userRatings ++ Seq(itemId, rating)
+      case None =>
+        userRatings.put(itemKey, Seq(itemId, rating))
+    }
+  }
+  
+  def insertItemRating(userId:Int, itemId:Int, rating:Int) {
+    val userKey = userId.toString()
+    
+    itemRatings.get(userKey) match {
+      case Some(itemRatings) =>
+        itemRatings ++ Seq(userId, rating)
+      case None =>
+        itemRatings.put(userKey, Seq(userId, rating))
+    }
+  }
+    
+  /*
+   * Ways to access information
+   */
   
   // Returns every user's rating for an item
-  def findRatingsForItem(item_id:Int):HashMap[Int, Int] = {
+  def findRatingsForItem(itemId:Int):HashMap[Int, Int] = {
     var ratings = new HashMap[Int, Int]
     
-    findUsersWhoRated(item_id) match {
-      case None =>
-        return ratings
-      case Some(users) =>
-        val user = users.iterator()
-        var user_id = user.nextDoc()
-        while(user_id != DocIdSetIterator.NO_MORE_DOCS) {
-          findRating(user_id, item_id) match {
-            case None =>
-              // do nothing?
-            case Some(rating) =>
-              ratings.put(user_id, rating)
-          }
-
-          user_id = user.nextDoc()
-        }
-    }
+    // findUsersWhoRated(itemId) match {
+    //   case None =>
+    //     return ratings
+    //   case Some(users) =>
+    //     val user = users.iterator()
+    //     var userId = user.nextDoc()
+    //     while(userId != DocIdSetIterator.NO_MORE_DOCS) {
+    //       findRating(userId, itemId) match {
+    //         case None =>
+    //           // do nothing?
+    //         case Some(rating) =>
+    //           ratings.put(userId, rating)
+    //       }
+    // 
+    //       userId = user.nextDoc()
+    //     }
+    // }
       
     return ratings
   }
   
   // Returs every item's rating from a user
-  def findRatingsForUser(user_id:Int):HashMap[Int, Int] = {
+  def findRatingsForUser(userId:Int):HashMap[Int, Int] = {
     var ratings = new HashMap[Int, Int]
     
-    findItemsRatedBy(user_id) match {
-      case None =>
-        return ratings
-      case Some(items) =>
-        val item = items.iterator()
-        var item_id = item.nextDoc()
-        while(item_id != DocIdSetIterator.NO_MORE_DOCS) {
-          findRating(user_id, item_id) match {
-            case None =>
-              // do nothing?
-            case Some(rating) =>
-              ratings.put(item_id, rating)
-          }
-
-          item_id = item.nextDoc()
-        }
-    }
+    // findItemsRatedBy(userId) match {
+    //   case None =>
+    //     return ratings
+    //   case Some(items) =>
+    //     val item = items.iterator()
+    //     var itemId = item.nextDoc()
+    //     while(itemId != DocIdSetIterator.NO_MORE_DOCS) {
+    //       findRating(userId, itemId) match {
+    //         case None =>
+    //           // do nothing?
+    //         case Some(rating) =>
+    //           ratings.put(itemId, rating)
+    //       }
+    // 
+    //       itemId = item.nextDoc()
+    //     }
+    // }
       
     return ratings
   }
   
   // Returns the set of users who rated the item
-  def findUsersWhoRated(item_id:Int):Option[DocSet] = {
-    indices.get(item_id.toString() + ":users")
+  def findUsersWhoRated(itemId:Int):Option[Set[Int]] = {
+    itemIndex.get(itemId.toString() + ":users")
   }
   
   // Returns the set of users who rated the item 'rating'
-  def findUsersWithRatings(item_id:Int, rating:Int):Option[DocSet] = {
-    indices.get(item_id.toString() + ":" + rating.toString())
+  def findUsersWithRatings(itemId:Int, rating:Int):Option[Set[Int]] = {
+    itemIndex.get(itemId.toString() + ":" + rating.toString())
   }
   
   // Returns the set of items rated by a user
-  def findItemsRatedBy(user_id:Int):Option[DocSet] = {
-    indices.get(user_id.toString() + ":items")
+  def findItemsRatedBy(userId:Int):Option[Set[Int]] = {
+    userIndex.get(userId.toString() + ":items")
   }
   
   // Returns the set of items rated 'rating' by the user
-  def findItemsWithRatings(user_id:Int, rating:Int):Option[DocSet] = {
-    indices.get(user_id.toString() + ":" + rating.toString())
-  }
-  
-  protected def insert(key:String, value:Int) {
-    indices.get(key) match {
-      case None => 
-        val docset = PForDeltaDocSetFactory.getPForDeltaDocSetInstance()
-        docset.addDoc(value)
-        indices.put(key, docset)
-      case Some(docset) => 
-        docset.find(value) match {
-          case false => 
-            docset.addDoc(value)
-          case true =>
-            // do nothing
-        }
-    }
+  def findItemsWithRatings(userId:Int, rating:Int):Option[Set[Int]] = {
+    userIndex.get(userId.toString() + ":" + rating.toString())
   }
 }
