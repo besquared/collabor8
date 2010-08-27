107b21dcf1e4430f74f1aca67d4833f1725fb383
Josh Ferguson <jferguson@yammer-inc.com>
2010-08-27 00:37:55 -0700

    bayesian filter base code

--- src/main/scala/com/yammer/c8/BayesianUserFilter.scala
+++ src/main/scala/com/yammer/c8/BayesianUserFilter.scala
@@ -1,11 +1,42 @@
 package com.yammer.c8
 
-import java.util._
+import scala.collection.mutable.Set
+import scala.collection.mutable.Seq
+import scala.collection.mutable.HashMap
 
-object BayesianUserFilter {
-  def classify(user_id:Int, items:ArrayList[Int], neighborhood:ArrayList[Int] = new ArrayList[Int], candidates:ArrayList[Int] = new ArrayList[Int]) {
+class BayesianUserFilter(ratings:RatingMatrix) {
+  def classify(userId:Int, itemIds:Set[Int], neighborhood:Set[Int], candidates:Set[Int]) {
+    // classify all items
+  }
+  
+  def classify(userId:Int, itemId:Int, neighborhood:Set[Int], candidates:Set[Int]) {
     val scores = new HashMap[Int, Double]
+    val userRatings = ratings.findItemsRatedBy(userId)
     
-    // find all users 
+    List(-1, 1).foreach(
+      classJ => {
+        var allRatingsCount = 0
+        var classRatingsCount = 0
+        val userClassRatings = ratings.findItemsWithRatings(userId, classJ)
+        
+        for((raterId, rating) <- ratings.findRatingsForItem(itemId)) {
+          if(userId != raterId && neighborhood.contains(raterId)) {
+            val raterRatings = ratings.findItemsRatedBy(raterId)
+            val raterClassRatings = ratings.findItemsWithRatings(raterId, classJ)
+            
+            allRatingsCount += raterRatings.size
+            classRatingsCount += raterClassRatings.size
+            
+            val similarRatings = raterRatings.getOrElse(Set.empty) & userRatings.getOrElse(Set.empty)
+            val matchingRatings = raterClassRatings.getOrElse(Set.empty) & userClassRatings.getOrElse(Set.empty)
+            
+            // With Laplacian Smoothness Correction
+            scores(classJ) *= (matchingRatings.size + 1) / (similarRatings.size + 2)
+          }
+        }
+        
+        scores(classJ) *= classRatingsCount / allRatingsCount
+      }
+    )
   }
 }
--- src/main/scala/com/yammer/c8/PearsonUserFilter.scala
+++ src/main/scala/com/yammer/c8/PearsonUserFilter.scala
@@ -1,4 +1,4 @@
 package com.yammer.c8
 
-object PearsonUserFilter {
+class PearsonUserFilter {
 }
